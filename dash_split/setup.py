33b014be1bd7c535f42252174cc77acb540bf0bd
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2022-03-15 14:47:07 +0100

    Update setup.py for readme etc.

--- dash_split/setup.py
+++ dash_split/setup.py
@@ -1,12 +1,16 @@
 import json
 import os
 from setuptools import setup
-
+from pathlib import Path
 
 with open('package.json') as f:
     package = json.load(f)
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
+
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name=package_name,
@@ -17,8 +21,10 @@ setup(
     license=package['license'],
     description=package.get('description', package_name),
     url="https://github.com/TCatshoek/dash-split",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[],
-    classifiers = [
+    classifiers=[
         'Framework :: Dash',
     ],    
 )
